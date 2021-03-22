75eb413ab549989de2bfea30de89241f6891c616
lusilucy <421445227@qq.com>
2021-03-23 00:19:31 +0800

    练习一：工资卡余额查询 E1_Moneyselect

--- Assignment/E1_Moneyselect/selectmoney.py
+++ Assignment/E1_Moneyselect/selectmoney.py
@@ -3,7 +3,7 @@ import sendmoney
 def select_money():
     month = int(input('请输入查询工资卡余额的月份：'))
     if month in range(0,13):
-        print('%s月工资卡余额为：%s' % (month,savemoney.save_money + sendmoney.send_money()[month]))
+        print('%s月工资卡余额为：%s' % (month, savemoney.save_money + sendmoney.send_money()[month]))
     else:
         print('输入月份有误，请输入输入数字0-12,0代表查询工资卡初始余额')
 
